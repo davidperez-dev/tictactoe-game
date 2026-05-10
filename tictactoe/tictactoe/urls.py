9220fc1ef70d3c2048f0298d30825a331c378a58
davidperez <davidperez.code@gmail.com>
2026-05-10 10:30:08 +0200

    [Added] API version query.

--- tictactoe/tictactoe/urls.py
+++ tictactoe/tictactoe/urls.py
@@ -18,8 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from tictactoe.views import StatusView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/status/", StatusView.as_view(), name="status"),
     path("api/v1/", include("users.urls")),
     path("api/v1/", include("game.urls")),
 ]
